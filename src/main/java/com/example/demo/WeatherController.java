176cea223abd1b8f1b83ded79e98b4ad8981ad20
Oscar Diaz <oscar01mx@gmail.com>
2025-09-17 18:33:07 -0600

    Adding Weather

--- src/main/java/com/example/demo/WeatherController.java
+++ src/main/java/com/example/demo/WeatherController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/weather")
@@ -9,4 +12,16 @@ public class WeatherController {
 
     private final String WEATHER_API  = "https://api.openweathermap.org/data/2.5/weather";
     private final String API_KEY      = "5a7876a58dcdee34f7cfd242e8126b33";
+
+    @GetMapping()
+    public String getMethodName(@RequestParam String latitude, @RequestParam String longitude) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = WEATHER_API + "?lat=" + latitude + "&lon=" + longitude 
+            + "&units=metric" + "&appid=" + API_KEY;
+        String result = restTemplate.getForObject(url, String.class);
+        if (result != null) {
+            return result;
+        }    
+        return "Error: Unable to fetch weather data.";
+    }   
 }
